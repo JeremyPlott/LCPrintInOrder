f504762293c62c51b43045e8b16590e4df83195b
jeremy_plott <plott.j@pg.com>
2020-01-07 10:26:45 -0500

    Add project files.

---
+++ LCPrintInOrder/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Foo
+{
+
+    public Foo()
+    {
+
+    }
+
+    bool firstComplete = false;
+    bool secondComplete = false;
+
+    public void First(Action printFirst)
+    {
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        firstComplete = true;
+    }
+
+    public void Second(Action printSecond)
+    {
+        while (!firstComplete)
+        {
+            System.Threading.Thread.Sleep(3);
+        }
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+
+        secondComplete = true;
+    }
+
+    public void Third(Action printThird)
+    {
+        while (!secondComplete)
+        {
+            System.Threading.Thread.Sleep(3);
+        }
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+}
